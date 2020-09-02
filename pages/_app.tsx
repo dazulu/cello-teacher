012e1194454f75aa5430ad7877489436c9bedf9a
dazulu <definitive@outlook.com>
2020-09-02 17:25:35 +0200

    feat(app): add _document.tsx Need to eject to add lang attribute

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -9,30 +9,6 @@ const MyApp = ({ Component, pageProps }: AppProps) => {
     <>
       <Head>
         <title>Christoph Siska</title>
-        <link rel="icon" href="/favicon.png" />
-
-        <link
-          rel="preconnect"
-          href="https://fonts.gstatic.com"
-          crossOrigin="anonymous"
-        />
-        <link
-          rel="preload"
-          as="style"
-          href="https://fonts.googleapis.com/css2?family=Merriweather:wght@300;400;700;900&display=swap"
-        />
-        <noscript>
-          <link
-            rel="stylesheet"
-            href="https://fonts.googleapis.com/css2?family=Merriweather:wght@300;400;700;900&display=swap"
-          />
-        </noscript>
-
-        <meta property="og:title" content="foo" />
-        <meta property="og:image" content="foo" />
-        <meta property="og:description" content="foo" />
-        <meta property="og:url" content="foo" />
-        <meta property="og:type" content="website" />
       </Head>
       <Component {...pageProps} />
     </>
---
+++ pages/_document.tsx
@@ -0,0 +1,53 @@
+import Document, {
+  Html,
+  Head,
+  Main,
+  NextScript,
+  DocumentContext,
+} from 'next/document'
+
+class MyDocument extends Document {
+  static async getInitialProps(ctx: DocumentContext) {
+    const initialProps = await Document.getInitialProps(ctx)
+    return { ...initialProps }
+  }
+
+  render() {
+    return (
+      <Html lang="de">
+        <Head>
+          <link rel="icon" href="/favicon.png" />
+
+          <link
+            rel="preconnect"
+            href="https://fonts.gstatic.com"
+            crossOrigin="anonymous"
+          />
+          <link
+            rel="preload"
+            as="style"
+            href="https://fonts.googleapis.com/css2?family=Merriweather:wght@300;400;700;900&display=swap"
+          />
+          <noscript>
+            <link
+              rel="stylesheet"
+              href="https://fonts.googleapis.com/css2?family=Merriweather:wght@300;400;700;900&display=swap"
+            />
+          </noscript>
+
+          <meta property="og:title" content="foo" />
+          <meta property="og:image" content="foo" />
+          <meta property="og:description" content="foo" />
+          <meta property="og:url" content="foo" />
+          <meta property="og:type" content="website" />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    )
+  }
+}
+
+export default MyDocument
