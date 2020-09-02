633902c0f9e46dd9e4a7defde998505286c419d0
dazulu <definitive@outlook.com>
2020-09-02 17:38:23 +0200

    feat(contact): add basic netlify form

--- components/contact.tsx
+++ components/contact.tsx
@@ -4,6 +4,38 @@ const contact = () => {
       <div id="contact" className="padding__wrapper">
         <div className="content__wrapper contact">
           <h2 className="title">Contact</h2>
+
+          <form
+            name="contact"
+            method="POST"
+            data-netlify="true"
+            data-netlify-recaptcha="true"
+          >
+            <p>
+              <label>
+                Name: <input type="text" name="name" />
+              </label>
+            </p>
+            <p>
+              <label>
+                Email: <input type="email" name="email" />
+              </label>
+            </p>
+            <p>
+              <label>
+                Phone Number: <input type="tel" name="phone" />
+              </label>
+            </p>
+            <p>
+              <label>
+                Message: <textarea name="message"></textarea>
+              </label>
+            </p>
+            <div data-netlify-recaptcha="true"></div>
+            <p>
+              <button type="submit">Send</button>
+            </p>
+          </form>
         </div>
       </div>
       <style jsx>{`
