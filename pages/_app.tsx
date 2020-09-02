329038adafd358e96ff914354dac80c910cb2b0e
dazulu <definitive@outlook.com>
2020-09-02 11:07:05 +0200

    feat(init): basic setup font, font perf and css reset

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,15 +1,44 @@
 import Head from 'next/head'
 import { AppProps } from 'next/app'
 
-import 'styles/globals.css'
+import Header from 'components/header'
+import Footer from 'components/footer'
+
+import 'styles/reset.scss'
+import 'styles/globals.scss'
 
 const MyApp = ({ Component, pageProps }: AppProps) => {
   return (
     <>
       <Head>
         <link rel="icon" href="/favicon.ico" />
+
+        <link
+          rel="preconnect"
+          href="https://fonts.gstatic.com"
+          crossOrigin="anonymous"
+        />
+        <link
+          rel="preload"
+          as="style"
+          href="https://fonts.googleapis.com/css2?family=Merriweather:wght@300;400;700;900&display=swap"
+        />
+        <noscript>
+          <link
+            rel="stylesheet"
+            href="https://fonts.googleapis.com/css2?family=Merriweather:wght@300;400;700;900&display=swap"
+          />
+        </noscript>
+
+        <meta property="og:title" content="foo" />
+        <meta property="og:image" content="foo" />
+        <meta property="og:description" content="foo" />
+        <meta property="og:url" content="foo" />
+        <meta property="og:type" content="website" />
       </Head>
+      <Header />
       <Component {...pageProps} />
+      <Footer />
     </>
   )
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,20 @@
+// import Intro from '/components/intro'
+// import About from '/components/about'
+// import Parallax from '/components/parallax'
+// import Faq from '/components/faq'
+// import Contact from '/components/contact'
+
 import styles from '../styles/Home.module.css'
 
 const Home = () => {
   return (
     <div className={styles.container}>
-      <main className={styles.main}>Cello</main>
+      {/* <Intro />
+      <About />
+      <Parallax background="bg1"/>
+      <Faq />
+      <Parallax background="bg2" />
+      <Contact /> */}
     </div>
   )
 }
