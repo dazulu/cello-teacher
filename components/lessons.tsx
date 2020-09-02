1d20a50c0b3acd05db9abb282712270660d8f209
dazulu <definitive@outlook.com>
2020-09-02 23:36:48 +0200

    feat(lessons): add section

---
+++ components/lessons.tsx
@@ -0,0 +1,21 @@
+const lessons = () => {
+  return (
+    <>
+      <div>
+        <div id="lessons" className="padding__wrapper">
+          <div className="content__wrapper">
+            <h2 className="title">Lessons</h2>
+          </div>
+        </div>
+      </div>
+      <style jsx>{`
+        .title {
+          text-align: center;
+          margin-bottom: 40px;
+        }
+      `}</style>
+    </>
+  )
+}
+
+export default lessons
