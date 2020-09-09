1e31d81d973538329fc71508c2c7e5771e3bad09
dazulu <definitive@outlook.com>
2020-09-09 19:46:48 +0200

    feat(netlify): wrap lambda function with handler

--- functions/delete-submissions.js
+++ functions/delete-submissions.js
@@ -5,21 +5,23 @@ const ACCESS_TOKEN = process.env.NETLIFY_API_ACCESS_TOKEN // custom Netlify env 
 
 const client = new NetlifyAPI(ACCESS_TOKEN)
 
-/*
- * Get an [] of submissions from all forms under SITE_ID
- * Loop through and delete each submission
- */
-client
-  .listSiteSubmissions({
-    site_id: SITE_ID,
-  })
-  .then((submissions) => {
-    if (submissions) {
-      submissions.forEach((submission) => {
-        client.deleteSubmission({ submission_id: submission.id })
-      })
-    }
-  })
-  .catch((err) => {
-    console.log(err)
-  })
+exports.handler = function (event, context) {
+  /*
+   * Get an [] of submissions from all forms under SITE_ID
+   * Loop through and delete each submission
+   */
+  client
+    .listSiteSubmissions({
+      site_id: SITE_ID,
+    })
+    .then((submissions) => {
+      if (submissions) {
+        submissions.forEach((submission) => {
+          client.deleteSubmission({ submission_id: submission.id })
+        })
+      }
+    })
+    .catch((err) => {
+      console.log(err)
+    })
+}
