ccffeef3dc3c06bd51860e2629385450f26776fd
dazulu <definitive@outlook.com>
2020-09-11 16:16:32 +0200

    refactor(netlify): form function - simplify code - fix obj ref

--- lambda-src/submission-created.js
+++ lambda-src/submission-created.js
@@ -1,6 +1,6 @@
 const NetlifyAPI = require('netlify')
 
-exports.handler = function (event, context, callback) {
+exports.handler = async function (event, context, callback) {
   /*
    * Get an [] of submissions from all forms under SITE_ID
    * Loop through and delete each submission
@@ -8,38 +8,26 @@ exports.handler = function (event, context, callback) {
 
   const client = new NetlifyAPI(process.env.NETLIFY_API_ACCESS_TOKEN)
 
-  const getSubmissions = () => {
-    return client
-      .listSiteSubmissions({
-        site_id: process.env.SITE_ID,
-      })
-      .catch((e) => console.log(e))
+  const submissions = await client
+    .listSiteSubmissions({
+      site_id: process.env.SITE_ID,
+    })
+    .catch((e) => console.log(e))
+
+  if (submissions.length) {
+    submissions.forEach((submission) => {
+      client
+        .deleteSubmission({ submission_id: submission.id })
+        .catch((e) => console.log(e))
+    })
+    callback(null, {
+      statusCode: 200,
+      body: 'Submissions deleted',
+    })
+  } else {
+    callback(null, {
+      statusCode: 200,
+      body: 'No submissions to delete',
+    })
   }
-
-  const deleteSubmission = (submission_id) => {
-    client
-      .deleteSubmission({ submission_id: submission_id })
-      .catch((e) => console.log(e))
-  }
-
-  ;(async () => {
-    const submissions = await getSubmissions()
-
-    if (submissions.length) {
-      submissions.forEach((submission) => {
-        deleteSubmission(submission.id)
-      })
-      callback(null, {
-        statusCode: 200,
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify({ msg: 'Submissions deleted' }),
-      })
-    } else {
-      callback(null, {
-        statusCode: 200,
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify({ msg: 'No submissions to delete' }),
-      })
-    }
-  })()
 }
