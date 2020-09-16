c64534a167e1be4dcd300d18637ac85f8ddb0f12
dazulu <definitive@outlook.com>
2020-09-17 00:01:25 +0200

    feat(analytics): add Fathom

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,9 @@
 import Head from 'next/head'
+import { useEffect } from 'react'
 import { AppProps } from 'next/app'
 import { Provider } from 'react-redux'
+import * as Fathom from 'fathom-client'
+import { useRouter } from 'next/router'
 
 import { useStore } from '../store'
 
@@ -8,7 +11,24 @@ import 'styles/reset.scss'
 import 'styles/globals.scss'
 
 const MyApp = ({ Component, pageProps }: AppProps) => {
+  const router = useRouter()
   const store = useStore(pageProps.initialReduxState)
+
+  useEffect(() => {
+    Fathom.load('NWNZTTVY', { url: 'https://bison.streamclocks.com/script.js' })
+
+    function onRouteChangeComplete() {
+      Fathom.trackPageview()
+    }
+
+    // Record a pageview when route changes
+    router.events.on('routeChangeComplete', onRouteChangeComplete)
+
+    // Unassign event listener
+    return () => {
+      router.events.off('routeChangeComplete', onRouteChangeComplete)
+    }
+  }, [])
 
   return (
     <>
